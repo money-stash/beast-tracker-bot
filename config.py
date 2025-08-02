fcb1a04364a5169c99bfc9fd83014ec7ef41f97f
maksEveline <maksimeveline@gmail.com>
2025-08-02 18:09:27 +0300

    fixed token

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 load_dotenv()
 
-TOKEN = "7682380679:AAFdq2roYQgRQX9XJu6fNpmH-sBVG2MMW0A"
+TOKEN = "7754876747:AAE0wnUih7111aqsGzb3wQFAAHrat6lg0gU"
 ADMIN_ID = [7742837753, 618500315]
 DB_PATH = "database/database.db"
 DATA_JSON = "database/data.json"
